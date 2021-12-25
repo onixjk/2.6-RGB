25c580d040f099fcf42ebc5c4af65ecbb4b61663
onixjk <onixjkk@gmail.ru>
2021-12-25 14:06:04 +0300

    Initial Commit

---
+++ HW2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.2
+//
+//  Created by Onix qq on 25.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
